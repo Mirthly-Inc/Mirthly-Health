a5b1883111dd53a05de4654485e1d3e2185eb212
Anand Prabhu <anandprabhu530@gmail.com>
2024-04-14 17:01:48 +0530

    Homepage Nav

--- mirthlyhealth/app/page.tsx
+++ mirthlyhealth/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <div>
-      <div>Test Commit</div>
+      <div className="flex w-full flex-col">
+        <div className="w-full justify-between p-4">
+          <div>Mirthly Health</div>
+          <div className="flex gap-4">
+            <button className="px-4 py-2 bg-black text-white rounded-xl">
+              Sign Up
+            </button>
+            <button className="px-4 py-2 bg-black text-white rounded-xl">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

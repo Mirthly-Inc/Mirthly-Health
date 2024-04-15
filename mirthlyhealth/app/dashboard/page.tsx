5a1a33bb0d488246bde2ceea5023913f1b517908
PremLearner <caprem3501@gmail.com>
2024-04-15 22:18:29 +0530

    ui changes

--- mirthlyhealth/app/dashboard/page.tsx
+++ mirthlyhealth/app/dashboard/page.tsx
@@ -1,5 +1,22 @@
-const page = () => {
-  return <div>page</div>;
+const Dashboard = () => {
+  return (
+    <div>
+      <div className='flex'>
+        <div className='basis-3/4 border-black border-2'>
+          <nav className='flex justify-between p-4'>
+            <div>Mirthly Health</div>
+            <div className='flex gap-4 '>
+              <div>about</div>
+              <div>contact</div>
+            </div>
+          </nav>
+        </div>
+        <div className='basis-1/4'>
+          <div>Graph</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Dashboard;

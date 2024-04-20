54cb5f8cc868174c81dad4c881a778f254d1b46b
PremLearner <caprem3501@gmail.com>
2024-04-20 10:14:31 +0530

    Calendar and logo

--- mirthlyhealth/app/layout.tsx
+++ mirthlyhealth/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { DataProvider } from "@/utils/dataContext";
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { DataProvider } from '@/utils/dataContext';
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+  subsets: ['latin'],
+  variable: '--font-sans',
 });
 export const metadata: Metadata = {
-  title: "Mirthly Health",
-  description: "Your Personalized mental health partner",
+  title: 'Mirthly Health',
+  description: 'Your Personalized mental health partner',
 };
 
 export default function RootLayout({
@@ -19,11 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <link rel='icon' href='/mirthly.jpg' type='image/jpg' sizes='any' />
+      </head>
       <DataProvider>
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            'min-h-screen bg-background font-sans antialiased',
             fontSans.variable
           )}
         >

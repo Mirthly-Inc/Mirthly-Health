cf704bec9e408335f1a61e70d581b06ca5da9e83
PremLearner <caprem3501@gmail.com>
2024-04-22 16:06:51 +0530

    firebase configurations

--- mirthlyhealth/components/SignUp_Form.tsx
+++ mirthlyhealth/components/SignUp_Form.tsx
@@ -3,8 +3,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { Button } from '@/components/ui/button';
+
 import {
   Form,
   FormControl,
@@ -14,48 +15,68 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { toast } from '@/components/ui/use-toast';
+import { Calendar } from '@/components/ui/calendar';
 import { Input } from '@/components/ui/input';
 import { ArrowRight } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { CalendarIcon } from 'lucide-react';
+import { format } from 'date-fns';
+import { auth, login } from '@/server';
 
 const formSchema = z
   .object({
+    name: z.string({ required_error: 'Please Enter Your Name' }),
     email: z.string({ required_error: 'Please Enter Email Address' }).email(),
     password: z.string().min(6, 'Password should be minimum 6 characters'),
     confirmPassword: z
       .string()
       .min(6, 'Password should be minimum 6 characters'),
-    dob: z.string(),
+    dob: z.date({
+      required_error: 'A date of birth is required.',
+    }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'Passwords do not match',
-    path: ['confirmPassword'], // path of error
+    path: ['confirmPassword'],
   });
-// .Refine(({ confirmPassword, password }, ctx) => {
-//   if (confirmPassword !== password) {
-//     ctx.addIssue({
-//       code: 'custom',
-//       message: 'The passwords did not match',
-//     });
-//   }
-// });
 
 export function SignUpForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: '',
       email: '',
       password: '',
       confirmPassword: '',
-      dob: '',
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const print = await login(values.email, values.password);
+    console.log(print);
+  };
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder='Enter Your Name' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name='email'
@@ -107,19 +128,47 @@ export function SignUpForm() {
           control={form.control}
           name='dob'
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Date of Birth</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder='Confirm Your Password'
-                  {...field}
-                  type='password'
-                />
-              </FormControl>
+            <FormItem className='flex flex-col'>
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={'outline'}
+                      className={cn(
+                        'w-[240px] pl-3 text-left font-normal',
+                        !field.value && 'text-muted-foreground'
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, 'PPP')
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className='w-auto p-0' align='start'>
+                  <Calendar
+                    mode='single'
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date('1900-01-01')
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <Button type='submit'>
           Start Your journey
           <ArrowRight className='ml-2' />
--- mirthlyhealth/server/index.ts
+++ mirthlyhealth/server/index.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, getFirestore } from 'firebase/firestore';
+import { User } from 'lucide-react';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD70YB2b2kW5Q9pofO1fvItemlXLFfcHI0',
@@ -9,7 +11,24 @@ const firebaseConfig = {
   messagingSenderId: '312940286579',
   appId: '1:312940286579:web:a5b36dd4693ba279819006',
 };
-
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
+const db = getFirestore(app);
+
+export const login = (email, password) =>
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+
+export const setUserData = async (user) => {
+  await setDoc(doc(db, 'Mirthly-Health', user.uid), {
+    dob: user.dob,
+    name: user.name,
+  });
+};

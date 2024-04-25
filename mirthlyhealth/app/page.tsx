3763399b50b1d558408073c20d018b4eb65946c7
PremLearner <caprem3501@gmail.com>
2024-04-25 20:27:20 +0530

    bckgrnd image

--- mirthlyhealth/app/page.tsx
+++ mirthlyhealth/app/page.tsx
@@ -1,9 +1,18 @@
-"use client";
+'use client';
+
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <div>
+        <Image
+          src='/home.jpg'
+          width={100}
+          height={100}
+          alt='home.jpg'
+          layout='fill'
+        ></Image>
         <div>Sample</div>
       </div>
     </div>
